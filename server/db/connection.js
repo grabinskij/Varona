ea17a947f6b21f46be9f5d9b5e029ab5a0e26bca
Bohdan Hrabynskyi <bogdan.grabinskij@gmail.com>
2025-01-17 15:46:55 +0100

    Enable ES Modules

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,12 +1,12 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        console.log('MongoDB connected');
-    })
-    .catch((err) => {
-        console.error('MongoDB connection error:', err);
-    });
+// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log('MongoDB connected');
+//     })
+//     .catch((err) => {
+//         console.error('MongoDB connection error:', err);
+//     });
